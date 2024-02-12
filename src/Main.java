ce261c995bcaed553bcdf02842b296d471bad4e7
Nikita Fedorov <pogdkeslg@MacBook-Pro-Nikita.local>
2024-02-12 11:25:55 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Выбирете действие");
+            System.out.println("1. Сравнение");
+            System.out.println("2. Выход");
+            int (str1, str2;) = scanner.nextInt();
+            double result;
+            if (person == 2) {
+                break;
+            } else if (person == 1) {
+                System.out.println("Введите значения первой строки: ");
+                int str1 = scanner.nextInt();
+            }
+
+        }
+    }
+}
